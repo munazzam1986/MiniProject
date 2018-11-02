acb9f2dd788cd7ad87308a84776e2c45d2a72a55
munazzam <munazzamm@gmail.com>
2018-11-02 19:38:10 -0400

    Feature Test: About page return 200 status code

--- tests/Feature/AboutTest.php
+++ tests/Feature/AboutTest.php
@@ -6,16 +6,16 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class LoginTest extends TestCase
+class AboutTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testLoginPage()
+    public function testAboutPage()
     {
-        $response = $this->get('/login');
+        $response = $this->get('/about');
 
         $response->assertStatus(200);
     }
