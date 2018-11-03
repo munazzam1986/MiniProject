0e53e4894a1dcb7dece439fe9714e9dedf226faf
munazzam <munazzamm@gmail.com>
2018-11-02 22:45:49 -0400

    Unit Test: Insert Car fixed code

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,30 +1,27 @@
 <?php
-
 namespace Tests\Unit;
+use App\Car;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\User;
-
-
-class UserTest extends TestCase
+class CarTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
+     * Test 9.  Unit test to insert new record to Cars Table in the database.
+     * New Car Record :
      * @return void
      */
-
     public function testInsertCar()
     {
+        //create a new instance of Car
         $car = new Car();
-
+        //Make
         $car-> make = 'Honda';
-
+        //Model
         $car-> model = 'Accord';
-
+        //Year
         $car-> year = 2011;
-
+        //test if the new user is saved in the database
         $this->assertTrue($car->save());
     }
 }
